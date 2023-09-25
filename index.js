5bc046db6733008b41cce967cdba954b9a8bbf56
michelle <junemichelle20@gmail.com>
2023-09-25 14:13:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,68 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value){
+  const updatedEmployee = { ...employee, [key]: value };
+  return updatedEmployee;
+}
+
+const employee = {
+name:"x",
+streetAddress:"y",
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+  employee[key]=value
+  return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+
+
+
+// const employee = {
+//   name: "Adline",
+//   position: "Software Engineer",
+//   age: 20,
+// };
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//   const updatedEmployee = { ...employee, [key]: value };
+//   return updatedEmployee;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//   employee[key] = value;
+//   return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//   const newEmployee = { ...employee };
+//   delete newEmployee[key];
+//   return newEmployee;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//   delete employee[key];
+//   return employee;
+// }
+
+
+// console.log(updateEmployeeWithKeyAndValue(employee, "age", 21));
+// console.log(employee); 
+
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "position", "Senior Software Engineer"));
+// console.log(employee);
+
+// console.log(deleteFromEmployeeByKey(employee, "age"));
+// console.log(employee); 
+
+// console.log(destructivelyDeleteFromEmployeeByKey(employee, "position"));
+// console.log(employee);
